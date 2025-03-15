a91b53a91dc762cb571d98d2b60b114204dfb340
nik4544 <150262010+nik4544@users.noreply.github.com>
2025-03-15 12:03:18 +0400

    ооо

--- 1.js
+++ 1.js
@@ -1,14 +1,56 @@
 console.log('hi');
-  const x = parseInt(prompt("Введите число:"))
-  sum = 0;
-  for ( let a = 1; a < x; a++) {
-    if(x % a === 0) {
-      sum += a;
-    }
- }
- if (sum > x){
-  console.log(x,'Избыточное');
- }
- else{
-  console.log(x,'Не избыточное');
- }
+
+x = parseInt(prompt("Введите число:"))
+y = parseInt(prompt("Введите число:"))
+z = parseInt(prompt("Введите число:"))
+/*
+if (x % 2 === 0) {
+    let func1 = (x) => expression;
+    console.log(x ** 2);
+}
+else {
+    let func2 = (x) => expression;
+    console.log(x ** 3);
+}
+if (y % 2 === 0) {
+    let func1 = (y) => expression;
+    console.log(y ** 2);
+}
+else {
+    let func2 = (y) => expression;
+    console.log(y ** 3);
+}
+if (z % 2 === 0) {
+    let func1 = (z) => expression;
+    console.log(z ** 2);
+}
+else {
+    let func2 = (z) => expression;
+    console.log(z ** 3);
+}
+*/
+
+if (x % 2 === 0) {
+    let func1 = (x) => x ** 2;
+    console.log(x);
+}
+else {
+    let func2 = (x) => x ** 3;
+    console.log(x);
+}
+if (y % 2 === 0) {
+    let func1 = (y) => y ** 2;
+    console.log(y);
+}
+else {
+    let func2 = (y) => y ** 3;
+    console.log(y);
+}
+if (z % 2 === 0) {
+    let func1 = (z) => z ** 2;
+    console.log(z);
+}
+else {
+    let func2 = (z) => z ** 3;
+    console.log(z);
+}
